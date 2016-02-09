0017469b787923bbee243a0a197b00fe16cf16db
Jonas Peter Hyatt <jph@dbc.dk>
2016-02-09 14:04:12 +0100

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# dbc-node-community-client
+
+[![David](https://img.shields.io/david/DBCDK/dbc-node-community-client.svg?style=flat-square)](https://david-dm.org/DBCDK/dbc-node-community-client#info=dependencies)
+[![David](https://img.shields.io/david/dev/DBCDK/dbc-node-community-client.svg?style=flat-square)](https://david-dm.org/DBCDK/dbc-node-community-client#info=devDependencies)
+
+Node client for the DBC community service.
+
---
+++ src/__tests__/client.test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import {assert} from 'chai';
+import ProfileClient from '../client.js';
+
+describe('Test methods in client.js', () => {
+  const methods = ProfileClient({endpoint: 'some_url'});
+  
+  /*
+  it('Should ensure createProfile is present', () => {
+    assert.isFunction(methods.createProfile, 'createProfile was found');
+  });
+
+  it('Should ensure getProfile is present', () => {
+    assert.isFunction(methods.getProfile, 'getProfile was found');
+  });
+
+  it('Should ensure updateProfile is present', () => {
+    assert.isFunction(methods.updateProfile, 'updateProfile was found');
+  });
+
+  it('Should ensure loginProfile is present', () => {
+    assert.isFunction(methods.loginProfile, 'loginProfile was found');
+  });
+
+  it('Should ensure logoutProfile is present', () => {
+    assert.isFunction(methods.logoutProfile, 'logoutProfile was found');
+  });
+
+  it('Should ensure saveLike is present', () => {
+    assert.isFunction(methods.saveLike, 'saveLike was found');
+  });
+
+  it('Should ensure updateLike is present', () => {
+    assert.isFunction(methods.updateLike, 'updateLike was found');
+  });
+
+  it('Should ensure removeLike is present', () => {
+    assert.isFunction(methods.removeLike, 'removeLike was found');
+  });
+  */
+
+  it('Should throw when no config object is provided to init', () => {
+    assert.throws(ProfileClient, Error, 'Expected config object but got null or no endpoint provided');
+  });
+
+  it('Should throw when config object is provided but no endpoint to init', () => {
+    assert.throws(() => ProfileClient({}), Error, 'Expected config object but got null or no endpoint provided');
+  });
+});
