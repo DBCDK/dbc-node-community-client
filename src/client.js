fd93bee4842cf7c634407e8c0c6ab4c0053ad7e2
Søren Vibjerg <vibjerg@gmail.com>
2016-02-15 15:18:49 +0100

    Add methods for gettting groups and adding posts

--- src/client.js
+++ src/client.js
@@ -67,6 +67,73 @@ function leaveGroup(endpoint, {uid, groupId, accessToken}) {
 }
 
 /**
+ * Fetches a Group in Loopback
+ */
+function getGroup(endpoint, params) {
+  return new Promise((resolve) => {
+    const id = params.id; // {include: ['owner', {comments: ['owner']}]}
+    const filter_str = JSON.stringify({include: [{posts: ['owner', {comments: ['owner']}]}, 'members']});
+    const url = endpoint + 'api/Groups/' + id + '?filter=' + filter_str;
+    request.get(
+      {
+        url: url
+      },
+      (err, httpResponse) => {
+        resolve(httpResponse);
+      }
+    );
+  });
+}
+
+/**
+ * Searches through Groups in Loopback
+ */
+function queryGroups(endpoint, params) {
+  return new Promise((resolve, reject) => {
+    const accessToken = params.accessToken;
+    var pattern = new RegExp('.*' + params.query + '.*', 'i');
+    const filter_str = JSON.stringify({where: {name: {regexp: pattern.toString()}}, include: ['members']});
+    const url = endpoint + 'api/Groups?access_token=' + accessToken + '&filter=' + filter_str;
+    request.get({url}, (err, res) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(res);
+    });
+  });
+}
+
+/**
+ * Create a Post
+ */
+function createGroupPost(endpoint, params) {
+  return new Promise((resolve, reject) => {
+    const accessToken = params.accessToken;
+    const groupId = params.groupId;
+    const url = endpoint + 'api/Groups/' + groupId + '/posts?access_token=' + accessToken;
+    const postBody = {
+      title: params.title,
+      content: params.content,
+      timeCreated: (new Date()).toUTCString(),
+      postownerid: params.postownerid
+    };
+
+    request.post({
+      url,
+      json: true,
+      body: postBody
+    }, (err, res) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(res);
+    });
+  });
+}
+
+
+/**
  * Setting the necessary paramerters for the client to be usable.
  * The endpoint is only set if endpoint is null to allow setting it through
  * environment variables.
@@ -86,6 +153,9 @@ export default function CommunityClient(config = null) {
     createProfile: createProfile.bind(null, config.endpoint),
     updateProfile: updateProfile.bind(null, config.endpoint),
     joinGroup: joinGroup.bind(null, config.endpoint),
-    leaveGroup: leaveGroup.bind(null, config.endpoint)
+    leaveGroup: leaveGroup.bind(null, config.endpoint),
+    getGroup: getGroup.bind(null, config.endpoint),
+    queryGroups: queryGroups.bind(null, config.endpoint),
+    createGroupPost: createGroupPost.bind(null, config.endpoint)
   };
 }
